3e93d3c6a975e618791cd5dc42c49a549e3e193c
jonathan-bug <jonathanlima.dev@gmail.com>
2026-09-01 14:08:43 -0600

    feat: agregar estructura productos

---
+++ app/Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+#[Fillable('barcode', 'name', 'sales_price', 'minimum_stock', 'is_active', 'category_id')]
+class Product extends Model
+{
+    public function cast(): array
+    {
+        return [
+            'is_active' => 'boolean'
+        ];
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
---
+++ database/migrations/2026_09_01_194317_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('barcode');
+            $table->string('name');
+            $table->decimal('sales_price', 10, 2);
+            $table->unsignedInteger('minimum_stock');
+            $table->boolean('is_active')->default(true);
+
+            $table->foreignId('category_id')
+                ->constrained()
+                ->restrictOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
